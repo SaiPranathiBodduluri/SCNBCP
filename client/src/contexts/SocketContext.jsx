933916062f887b4d1099b8b013d2a81a151b621f
SaiPranathiBodduluri <saipranathibodduluri@gmail.com>
2025-11-08 12:19:01 +0530

    Update MongoDB Atlas connection link and backend setup

--- client/src/contexts/SocketContext.jsx
+++ client/src/contexts/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const newSocket = io('http://localhost:5000');
+      const newSocket = io('https://scnbcp-backend.onrender.com');
       
       newSocket.on('connect', () => {
         console.log('Connected to server');
